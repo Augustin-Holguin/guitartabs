98f36cd85b5b712a27e07dbc25ec86598e96fdc0
gusholguin <augustin.holguin@gmail.com>
2018-02-28 17:30:58 +0100

    sql database setup using sequelize

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="">
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="email"><br>
-    <input type="password" name="password" v-model="password" placeholder="password"><br>
-    <button @click="register">Sign up</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" v-model="email" placeholder="email"><br>
+          <input type="password" name="password" v-model="password" placeholder="password"><br>
+          <div class="error" v-html="error"></div><br>
+          <v-btn class="sign" @click="register">Sign up</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -13,16 +23,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -30,5 +44,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  margin-top: 10px;
+  color: crimson;
+  font-size: 0.9em;
+}
 </style>
