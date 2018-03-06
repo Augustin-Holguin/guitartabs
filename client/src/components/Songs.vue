89ed111a5428b8767736c67fe1bf6ee3714c0356
gusholguin <augustin.holguin@gmail.com>
2018-03-06 18:15:13 +0100

    song component setup

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,18 +2,32 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-
+        <div v-for="song in songs" :key="song.title">
+          {{ song.title }} -
+          <b style="color:crimson">{{ song.artist }}</b> -
+          {{ song.album }}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    // do a request to the backend for all the songs
+    this.songs = await SongsService.index()
   }
 }
 </script>
