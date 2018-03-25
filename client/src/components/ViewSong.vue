658d22f8efe7409be64ca248a45934225bdc8e5d
gusholguin <augustin.holguin@gmail.com>
2018-03-25 19:07:37 +0200

    song view 2.0

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,34 +1,56 @@
 <template>
-  <v-layout>
-    <v-flex xs6 class="mt-2 ml-1">
-      <panel title= "Song Metadata">
-        <v-layout>
-          <v-flex xs6>
-            <div class="song-title">
-              {{ song.title }}
-            </div>
-            <div class="song-artist">
-              {{ song.artist }}
-            </div>
-            <div class="song-genre">
-              {{ song.genre }}
-            </div>
-          </v-flex>
+  <div>
+    <v-layout>
+      <v-flex xs6 class="mt-2 ml-1">
+        <panel title= "Song Metadata">
+          <v-layout>
+            <v-flex xs6 class="song-details">
+              <div class="song-title">
+                {{ song.title }}
+              </div>
+              <div class="song-artist">
+                {{ song.artist }}
+              </div>
+              <div class="song-genre">
+                {{ song.genre }}
+              </div>
+              <div class="song-album">
+                {{ song.album }}
+              </div>
+            </v-flex>
 
-          <v-flex xs6>
-            <img class="album-image" :src="song.albumImageUrl"/>
-            <br>
-            {{ song.album }}
-          </v-flex>
-        </v-layout>
-      </panel>
-    </v-flex>
-    <v-flex xs6 class="ml-3 mt-2 mr-1">
-      <panel title="Tabs">
-        {{ song.tab }}
-      </panel>
-    </v-flex>
-  </v-layout>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageUrl"/>
+            </v-flex>
+          </v-layout>
+        </panel>
+      </v-flex>
+      <v-flex xs6 class="ml-3 mt-2 mr-1">
+        <panel title="Tabs">
+          <textarea
+            readonly
+            v-model="song.tab">
+          </textarea>
+        </panel>
+      </v-flex>
+    </v-layout>
+
+    <v-layout>
+      <v-flex xs6 class="mt-2 ml-1">
+        <panel title="Video">
+
+        </panel>
+      </v-flex>
+      <v-flex xs6 class="ml-3 mt-2 mr-1">
+        <panel title="Lyrics">
+          <textarea
+            readonly
+            v-model="song.lyrics">
+          </textarea>
+        </panel>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -59,6 +81,10 @@ export default {
   overflow: hidden;
 }
 
+.song-details {
+  margin: auto;
+}
+
 .song-title {
   font-size: 28px;
   font-weight: bold;
@@ -73,8 +99,24 @@ export default {
   font-size: 14px;
 }
 
+.song-album {
+  margin-top: 10px;
+  font-size: 18px;
+  opacity: 0.7;
+}
+
 .album-image {
   width: 60%;
   margin: 0 auto;
+}
+
+textarea {
+  width: 100%;
+  height: 300px;
+  border: none;
+  font-family: monospace;
+  border-style: none;
+  overflow: auto;
+  border-color: transparent;
 }
 </style>
