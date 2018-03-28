073a56e5a1219aa5c17d41a27cd8ae9b59fa573c
gusholguin <augustin.holguin@gmail.com>
2018-03-28 21:07:10 +0200

    quick refactoring

--- client/src/components/Songs/SongsSearchPanel.vue
+++ client/src/components/Songs/SongsSearchPanel.vue
@@ -1,12 +1,21 @@
 <template>
   <panel title="Search">
-
+    <v-text-field label="Search by song, artist, album or genre" v-model="search"></v-text-field>
   </panel>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      search: ''
+    }
+  },
+  watch: {
+    search (value) {
+      console.log(value)
+    }
+  }
 }
 </script>
 
