03b423508f45de1193ce68d97b278f76f9ce17b2
gusholguin <augustin.holguin@gmail.com>
2018-03-30 12:27:32 +0200

    search panel working

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -1,12 +1,30 @@
 const {Song} = require('../models')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 module.exports = {
   async index (req, res) {
     try {
-      const song = await Song.findAll({
-        limit: 10                   // index method which is going to find all songs in db and limit it to 10 and return that back in the pave load
-      })
-      res.send(song)
+      let songs = null
+      const search = req.query.search
+      if (search) {
+        songs = await Song.findAll({
+          where: {
+            [Op.or]: [
+              'title', 'artist', 'genre', 'album'
+            ].map(key => ({
+              [key]: {
+                [Op.like]: `%${search}%`
+              }
+            }))
+          }
+        })
+      } else {
+        songs = await Song.findAll({
+          limit: 10                   // index method which is going to find all songs in db and limit it to 10 and return that back in the pave load
+        })
+      }
+      res.send(songs)
     } catch (err) {
       res.status(500).send({
         error: 'An error has occured while fetching the songs' // ex: email already exists
